09a66cbf8cac3127579badd85f659f394bb90eec
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2018-02-10 14:04:43 +0000

    fix typos

--- tier-1/terraform/main.tf
+++ tier-1/terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "default" {
 }
 
 # Grant the VPC internet access on its main route table
-resource "aws_route" "inte/net_access" {
+resource "aws_route" "internet_access" {
   route_table_id         = "${aws_vpc.default.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.default.id}"
